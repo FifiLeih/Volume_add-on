4df2fc9ef9e76ccade935ac09e4d59499d0fed09
fifileih <mpolyakov99@gmail.com>
2024-03-03 16:52:26 +0100

    New

---
+++ app/src/main/java/com/example/rotaryencoder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rotaryencoder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Rotary().volume()
+    }
+}
